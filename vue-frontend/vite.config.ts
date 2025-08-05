abeec143841f5c48a78313cc945067a32a73591e
Abel Camilo Yi Martínez <abelyimartinez1987@hotmail.com>
2025-08-05 15:43:13 -0500

    Configuration of vitest, especially for test exclusions

--- vue-frontend/vite.config.ts
+++ vue-frontend/vite.config.ts
@@ -1,11 +1,31 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vuetify from 'vite-plugin-vuetify'
+// vite.config.ts
+import { defineConfig } from "vitest/config";
+import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({ autoImport: true }),
-  ],
-})
+  plugins: [vue(), vuetify({ autoImport: true })],
+  test: {
+    exclude: [
+      "node_modules",
+      "dist",
+      "**/interfaces/**",
+      "**/main.ts",
+      "**/App.vue",
+      "**/router/**",
+      "**/*.d.ts",
+    ],
+    coverage: {
+      reporter: ["text", "html", "lcov"],
+      exclude: [
+        "src/interfaces/**",
+        "src/constants/**",
+        "src/main.ts",
+        "src/App.vue",
+        "src/router/**",
+        "**/*.d.ts",
+        "vite.config.ts"
+      ],
+    },
+  },
+});
