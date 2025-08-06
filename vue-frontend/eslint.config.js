c23af97a1f156174a9f4196163a87d08f6642843
Abel Camilo Yi Martínez <abelyimartinez1987@hotmail.com>
2025-08-06 13:07:54 -0500

    Eslint installation and configuration

---
+++ vue-frontend/eslint.config.js
@@ -0,0 +1,48 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import json from "@eslint/json";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+  },
+  tseslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.{ts,tsx,vue}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        extraFileExtensions: [".vue"],
+      },
+    },
+  },
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.jsonc"],
+    plugins: { json },
+    language: "json/jsonc",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.json5"],
+    plugins: { json },
+    language: "json/json5",
+    extends: ["json/recommended"],
+  },
+]);
