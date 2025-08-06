898b92edd892824aeac5e91cb64b835b114c978a
Abel Camilo Yi Martínez <abelyimartinez1987@hotmail.com>
2025-08-06 13:21:30 -0500

    update ESLint

--- vue-frontend/eslint.config.js
+++ vue-frontend/eslint.config.js
@@ -4,8 +4,10 @@ import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
 import json from "@eslint/json";
 import { defineConfig } from "eslint/config";
+import vueParser from "vue-eslint-parser";
 
 export default defineConfig([
+  // Pure JS
   {
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
@@ -14,19 +16,41 @@ export default defineConfig([
       globals: { ...globals.browser, ...globals.node },
     },
   },
+
+  // TypeScript base rules
   tseslint.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
+
+  // Base configuration for Vue
   pluginVue.configs["flat/essential"],
+
+  // TypeScript in .ts and .tsx
   {
-    files: ["**/*.{ts,tsx,vue}"],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
         project: "./tsconfig.eslint.json",
-        extraFileExtensions: [".vue"],
+        sourceType: "module",
       },
     },
   },
+
+  // Vue: use vue-eslint-parser + typescript-eslint internally
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tseslint.parser,
+        project: "./tsconfig.eslint.json",
+        extraFileExtensions: [".vue"],
+        sourceType: "module",
+      },
+    },
+  },
+
+  // JSON files
   {
     files: ["**/*.json"],
     plugins: { json },
